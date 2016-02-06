e9b2316a7a8ad94ebc3accc0ae8bbbf43f2bf38b
Marcel Kiss <marcel.kiss0@gmail.com>
2016-02-06 14:19:59 +0100

    Fix reflextion of boolean

--- joogar/src/main/java/net/skoumal/joogar/shared/util/ReflectionUtils.java
+++ joogar/src/main/java/net/skoumal/joogar/shared/util/ReflectionUtils.java
@@ -72,8 +72,10 @@ public abstract class ReflectionUtils {
                         columnType.equals(Float.class) || columnType.equals(float.class) ||
                         columnType.equals(Double.class) || columnType.equals(double.class)) {
                     return columnValue;
-                } else if (columnType.equals(Boolean.class) || columnType.equals(boolean.class)) {
+                } else if (columnType.equals(Boolean.class)) {
                     return ((Boolean) columnValue) ? 1 : 0;
+                } else if (columnType.equals(boolean.class)) {
+                    return ((boolean) columnValue) ? 1 : 0;
                 } else if (columnType.getName().equals("[B")) {
                     return columnValue;
                 } else if (columnType.isEnum()) {
