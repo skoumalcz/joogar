39e4cfeeb41fae9510f4f1b4a3db5c97a94073a3
Vladislav Gingo Skoumal <skoumal@skoumal.net>
2016-06-08 23:25:52 +0200

    WAL tests

--- joogar/src/androidTest/java/net/skoumal/joogar/integration/WalTests.java
+++ joogar/src/androidTest/java/net/skoumal/joogar/integration/WalTests.java
@@ -11,11 +11,31 @@ public class WalTests extends CrudTestCase {
 
     public void testParallelReadDuringTransaction() {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            final String preTransactionString = "Pre-transaction String";
+            StringFieldExtendedModel preTransactionModel = new StringFieldExtendedModel(preTransactionString);
+            final long preTransactionId = preTransactionModel.save();
+
             JoogarRecord.openTransaction();
 
             String string = "Test String";
             StringFieldExtendedModel model = new StringFieldExtendedModel(string);
             long id = model.save();
+
+            Thread readThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    StringFieldExtendedModel query = JoogarRecord.findById(StringFieldExtendedModel.class, preTransactionId);
+                    assertEquals(preTransactionString, query.getString());
+                }
+            });
+            readThread.start();
+            try {
+                readThread.join(1000);
+            } catch (InterruptedException e) {
+                fail("Join was interrupted.");
+            }
+
+            assertFalse(readThread.isAlive());
 
             JoogarRecord.commitTransaction();
 
